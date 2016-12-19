f54886d9e1439dd68de03f295409545630344a39
Rishi Karri <rishikarri1@gmail.com>
2016-12-19 15:11:35 -0500

    Initial Commit

---
+++ stock-quote-generator/scripts.js
@@ -0,0 +1,12 @@
+
+
+$(document).ready(function(){
+
+
+	// let's create functionality for the click button
+	$("#get-stocks").click(function(){
+		event.preventDefault();
+		console.log("it's lit!")
+	});
+});
+
